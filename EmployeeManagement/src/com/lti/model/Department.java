1ec3d155968d0eef97b058e4d08b0a8532ade175
arkamniax <arkamniax@users.noreply.github.com>
2020-10-15 13:22:44 +0530

    added department

---
+++ EmployeeManagement/src/com/lti/model/Department.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Department {
+	String depName;
+	int depId;
+	public String getDepName() {
+		return depName;
+	}
+	public void setDepName(String depName) {
+		this.depName = depName;
+	}
+	public int getDepId() {
+		return depId;
+	}
+	public void setDepId(int depId) {
+		this.depId = depId;
+	}
+	
+}
